2167dbf5cb9e80d70692041e7a7a10ca20c49c5f
TmpName <gihubtmp@yopmail.com>
2020-04-01 11:05:06 +0000

    Create up2stream.js

---
+++ up2stream.js
@@ -0,0 +1,233 @@
+/**
+
+Trouvé a https://uptostream.com/api/streaming/source/get?file_code=xa7l7dkao8125
+
+*/
+var _0x44bf = ["W8KTw7Aw", "wqnDsETCmA==", "wr9uX3c=", "wpTCkyfCtg==", "IcOxFkc=", "QsOww4bClQ==", "fV9sPw==", "Rzlew7k=", "w4xUOAo=", "wrxtVWs=", "YwJvw4w=", "w7XCksO9w40=", "K8KIcUA=", "ZsKLwrrCtA==", "woYcw7QE", "SxJ5w70=", "w4xVOAo=", "esKgAsKQ", "woV/N1w=", "ajIAw58=", "wonDo8OSwrI=", "UMKFYMKq", "CMKmwpE=", "O1/DhsKj", "wqdgS2g=", "w7XCscOgwqo=", "bBodfQ==", "wp5xKRk=", "UcOmw6LDog==", "fwQIAg==", "FcKhw4jCmw==", "wrPCu8K9", "wrNgwojCrA==", "Y8K3w7PDrQ==", "wq/DtHXClA==", "VsKzwr7CuQ==", "aD8uw6o=", 
+"FMOxWQ==", "GmNGTg==", "w7TDiwfDvQ==", "wpwUw74G", "f18TfA==", "LcOQwpQg", "NGZrfA==", "ASrCtg==", "cDxxcA==", "wqFfwr3CiA==", "w6vCqMOEwqE=", "SsKkw4zChw==", "GsKrMcOO", "w6LCkDpa", "HAvDhgs=", "wr16Vw==", "w4XDlkLCog==", "dSBqw7o=", "dGJjJw==", "Uhh7w7A=", "PcOkVsKP", "IHxafQ==", "Z1dqPw==", "ScK1f8KI", "wqUES8Kt", "B8Kjw6PCnQ==", "wp/ChD7Csw==", "DmzCmXQ=", "dcKIccOR", "MGpudA=="];
+(function(data, i) {
+
+  var write = function(isLE) {
+    for (; --isLE;) {
+      data["push"](data["shift"]());
+    }
+  };
+  write(++i);
+})(_0x44bf, 129);
+
+
+//zzz a remplacer par _0x223d
+
+var _0x223d = function(n, dn) {
+
+
+  n = n - 0;
+  var i = _0x44bf[n];
+  if (_0x223d["ypAAoZ"] === undefined) {
+    (function() {
+
+      var unescape = function() {
+        var source;
+        try {
+          source = Function("return (function() " + '{}.constructor("return this")( )' + ");")();
+        } catch (_0x25f836) {
+          source = window;
+        }
+        return source;
+      };
+      var s_utf8 = unescape();
+      var listeners = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
+      if (!s_utf8["atob"]) {
+
+        s_utf8["atob"] = function(i) {
+          var str = String(i)["replace"](/=+$/, "");
+          var pix_color = "";
+          var bc = 0;
+          var bs;
+          var buffer;
+          var Y = 0;
+          for (; buffer = str["charAt"](Y++); ~buffer && (bs = bc % 4 ? bs * 64 + buffer : buffer, bc++ % 4) ? pix_color = pix_color + String["fromCharCode"](255 & bs >> (-2 * bc & 6)) : 0) {
+            buffer = listeners["indexOf"](buffer);
+          }
+          return pix_color;
+        };
+      }
+    })();
+
+    var testcase = function(data, fn) {
+      var secretKey = [];
+      var y = 0;
+      var temp;
+      var testResult = "";
+      var tempData = "";
+      data = atob(data);
+      var val = 0;
+      var key = data["length"];
+      for (; val < key; val++) {
+        tempData = tempData + ("%" + ("00" + data["charCodeAt"](val)["toString"](16))["slice"](-2));
+      }
+
+      data = decodeURIComponent(tempData);
+      var x;
+      x = 0;
+      for (; x < 256; x++) {
+        secretKey[x] = x;
+      }
+      x = 0;
+      for (; x < 256; x++) {
+        y = (y + secretKey[x] + fn["charCodeAt"](x % fn["length"])) % 256;
+        temp = secretKey[x];
+        secretKey[x] = secretKey[y];
+        secretKey[y] = temp;
+      }
+      x = 0;
+      y = 0;
+      var i = 0;
+      for (; i < data["length"]; i++) {
+        x = (x + 1) % 256;
+        y = (y + secretKey[x]) % 256;
+        temp = secretKey[x];
+        secretKey[x] = secretKey[y];
+        secretKey[y] = temp;
+        testResult = testResult + String["fromCharCode"](data["charCodeAt"](i) ^ secretKey[(secretKey[x] + secretKey[y]) % 256]);
+      }
+      return testResult;
+    };
+
+    _0x223d["IORbWK"] = testcase;
+    _0x223d["oCPMnu"] = {};
+    _0x223d["ypAAoZ"] = !![];
+  }
+
+  var b = _0x223d["oCPMnu"][n];
+  if (b === undefined) {
+    if (_0x223d["DvatpI"] === undefined) {
+      _0x223d["DvatpI"] = !![];
+    }
+    i = _0x223d["IORbWK"](i, dn);
+    _0x223d["oCPMnu"][n] = i;
+  } else {
+    i = b;
+  }
+  return i;
+};
+
+var sources = [];
+sources[_0x223d("0x2e", "NJ[m")](function parse() {
+
+  function build_extension_ui() {
+    return map[_0x223d("0x9", "V0IK") + "x"];
+  }
+
+  function parseSequence() {
+    var newAttr = {
+      "coYVe" : _0x223d("0x0", "mcAP") + _0x223d("0x1f", "KnSr") + _0x223d("0x3", "3d%@") + "3.up" + "tost" + _0x223d("0x4", "*8ps") + _0x223d("0x2f", "5it[")
+    };
+    if (map["yJyt" + "D"](_0x223d("0x16", "7bbz") + "V", map[_0x223d("0x1a", "3d%@") + "z"])) {
+      return "0";
+    } else {
+      return newAttr[_0x223d("0x6", "r52j") + "e"];
+    }
+  }
+  var map = {
+    "xhFrx" : _0x223d("0x1e", "0aSa") + _0x223d("0x20", "sc#R") + "0",
+    "oLyYU" : function(value, joiner) {
+      return value !== joiner;
+    },
+    "qfkqq" : _0x223d("0xc", "A$MN") + "m",
+    "JfRJS" : "http" + _0x223d("0x21", "[Cg^") + _0x223d("0x3", "3d%@") + _0x223d("0x12", "Q7c(") + _0x223d("0x28", "[Cg^") + _0x223d("0xe", "y*NM") + _0x223d("0x23", "nCGJ"),
+    "KJyon" : _0x223d("0x30", "*8ps") + _0x223d("0x2", "kHCo") + "4",
+    "SlPGm" : function(name, initialValue) {
+      return name === initialValue;
+    },
+    "OFJNB" : "KFvc" + "D",
+    "rXTUX" : _0x223d("0x2b", "*8ps") + "x",
+    "ekhZd" : "1w3q" + "y2ac" + _0x223d("0x24", "0aSa"),
+    "bsUdr" : _0x223d("0x31", "atbQ"),
+    "yJytD" : function(value, joiner) {
+      return value !== joiner;
+    },
+    "RzfMz" : _0x223d("0x25", "cj[r") + "P",
+    "GEOWK" : function(value, joiner) {
+      return value !== joiner;
+    },
+    "yDPuH" : _0x223d("0x3b", "A$MN") + "R",
+    "ZNLwZ" : function(formatters, customFormatters) {
+      return formatters + customFormatters;
+    },
+    "kwOeK" : function(formatters, customFormatters) {
+      return formatters + customFormatters;
+    },
+    "mAWlW" : function(formatters, customFormatters) {
+      return formatters + customFormatters;
+    },
+    "jaSDp" : function(saveNotifs) {
+      return saveNotifs();
+    },
+    "pbhZH" : function(saveNotifs) {
+      return saveNotifs();
+    },
+    "kBQuj" : function(saveNotifs) {
+      return saveNotifs();
+    },
+    "esrdY" : function(saveNotifs) {
+      return saveNotifs();
+    },
+    "SZEPr" : _0x223d("0x2d", "h3&k") + _0x223d("0x15", "^G%U") + "4",
+    "KZlLf" : _0x223d("0x3a", "@ynJ")
+  };
+
+  var keyboard = function() {
+    var blocklights = {
+      "LAKLs" : map[_0x223d("0x33", "cj[r") + "x"]
+    };
+    if (map[_0x223d("0x29", "cIKh") + "U"](map[_0x223d("0x7", "y*NM") + "q"], map[_0x223d("0x35", "nCGJ") + "q"])) {
+      return blocklights[_0x223d("0xf", "iz]m") + "s"];
+    } else {
+      return map[_0x223d("0x14", "A$MN") + "S"];
+    }
+  };
+
+  var data = function() {
+    if (map[_0x223d("0x3d", "A$MN") + "m"](map["OFJN" + "B"], map[_0x223d("0x3e", "ge5C") + "X"])) {
+      return map[_0x223d("0x41", "3d%@") + "n"];
+    } else {
+      return map[_0x223d("0x27", "r52j") + "d"];
+    }
+  };
+
+  var node = function() {
+    return map[_0x223d("0x3f", "*8ps") + "r"];
+  };
+
+  var parse = function() {
+    var bitmap = {
+      "HqqLw" : "1w3q" + _0x223d("0xa", "nCGJ") + _0x223d("0x1b", "nCGJ")
+    };
+    if (map[_0x223d("0x38", "ZMWa") + "K"](map[_0x223d("0x36", "UGmV") + "H"], map[_0x223d("0x42", "&rx^") + "H"])) {
+      return bitmap[_0x223d("0x32", "b^xa") + "w"];
+    } else {
+      return _0x223d("0xb", "Buvi") + _0x223d("0xd", "^G%U") + "4";
+    }
+  };
+  let src = {};
+  var dns_labeltype_bitstring = map[_0x223d("0x19", "5gBF") + "Z"](map["ZNLw" + "Z"](map[_0x223d("0x11", "g!DZ") + "Z"](map[_0x223d("0x37", "wbEl") + "Z"](map[_0x223d("0x34", "0aSa") + "K"](map[_0x223d("0x18", "cIKh") + "W"](map[_0x223d("0x22", "mUo#") + "W"](map[_0x223d("0x2c", "vXTM") + "p"](keyboard), "/") + map["pbhZ" + "H"](data), "/"), map[_0x223d("0x26", "p0qg") + "j"](node)), "/"), map[_0x223d("0x3c", "KnSr") + "j"](parseSequence)), "/"), map[_0x223d("0x1c", "x1j2") + "Y"](parse));
+  var i = map[_0x223d("0x10", "fd!(") + "r"];
+  var ret = map[_0x223d("0x1d", "x&UR") + "f"];
+  var l = map[_0x223d("0x8", "@ynJ") + "r"];
+
+  var _0x1597f3 = "unkn" + "own " + "0";
+
+  var nbd = "0";
+  src[_0x223d("0x39", "y*NM")] = dns_labeltype_bitstring;
+  src[_0x223d("0x17", "sc#R")] = i;
+  src[_0x223d("0x40", "Buvi") + "l"] = ret;
+  src[_0x223d("0x2a", "*oFa")] = l;
+  src[_0x223d("0x13", "h3&k")] = map[_0x223d("0x5", "nHR]") + "x"];
+
+  src[_0x223d("0x1", "@ynJ") + "ng"] = nbd;
+  
+  window.alert(dns_labeltype_bitstring);
+  
+  return src;
+}());
